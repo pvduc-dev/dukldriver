7530cc137f85866b524c220fa02eaf8e4d62720d
PvDuc <pvduc.dev@gmail.com>
2025-06-09 02:54:18 +0700

    update in macoos

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -3,7 +3,7 @@ import UIKit
 import FirebaseCore
 import FirebaseMessaging
 
-@UIApplicationMain
+@main
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
